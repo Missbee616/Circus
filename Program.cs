0bd2aee14890aa0328a2607341f6a39ac22b9bb8
othelle <othellemcallister@gmail.com>
2017-08-25 15:21:36 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Deliverable1
+{
+
+    //A program to read two integers from the user and check if corresponding place 
+    //in the two numbers sums to the same total
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumber = "";
+            string secondNumber = "";
+
+            Console.Write("Enter any number:");
+            firstNumber = Console.ReadLine();
+            Console.WriteLine("The first number is " +firstNumber);
+            Console.WriteLine("Length of first number is " + firstNumber.Length);
+
+            Console.Write("Enter another number with the same number of digits:");
+            secondNumber = Console.ReadLine();
+            Console.WriteLine("The second number is " + secondNumber);
+            Console.WriteLine("Length of second number is " + secondNumber.Length);
+
+            
+            {
+
+            }
+        }
+
+    }
+   
+}
